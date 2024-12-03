ef88e8822a8db03026ee86dfc2f4044270235a89
Shruthisrivastav <shruthisrivastav05@gmail.com>
2024-12-03 15:01:56 +0530

    Update Agee.java

--- Agee.java
+++ Agee.java
@@ -2,21 +2,18 @@ import java.util.Scanner;
 
 public class Agee {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter your birth year (YYYY): ");
+            int birthYear = scanner.nextInt();
 
-        System.out.println("Enter your birth year (YYYY): ");
-        int birthYear = scanner.nextInt();
+            int currentYear = java.time.Year.now().getValue();
 
-        int currentYear = java.time.Year.now().getValue();
-
-        if (birthYear > currentYear) {
-            System.out.println("Birth year cannot be in the future!");
-        } else {
-            int age = currentYear - birthYear;
-            System.out.println("You are " + age + " years old.");
+            if (birthYear > currentYear) {
+                System.out.println("Birth year cannot be in the future!");
+            } else {
+                int age = currentYear - birthYear;
+                System.out.println("You are " + age + " years old.");
+            }
         }
-
-        scanner.close();
     }
 }
-
